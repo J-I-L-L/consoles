66891fc26d79af20ffb5d5e76d99b914463ae1a1
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-11-30 18:45:27 +0300

    zad2_IV

---
+++ zad2IV/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace zad2IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=4; i<=7; i++)
+            {
+                for(int j=1; j<=9; j++)
+                {
+                   
+                    Console.Write(" "+i + j+" ");
+                }
+                Console.WriteLine(" "+(i+1)+"0");
+            }
+        }
+    }
+}
