1cb11de50e307f10c6e92ab3577d36ea406362bf
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-11-30 18:44:49 +0300

    zad2_III

---
+++ 2zadIII/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2zadIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выведение чисел в определённом порядке\n\n");
+            Console.WriteLine("Способ через While:\n");
+            int i = 25;
+            while (i <= 35)
+            {
+                Console.WriteLine(i + "  " +(i+0.5) + "  " +(i-0.2));
+                i++;
+            }
+            Console.WriteLine("\nСпособ через Do While:\n");
+            int j = 25;
+            do
+            {
+                Console.WriteLine(j + "  " + (j + 0.5) + "  " + (j - 0.2));
+                j++;
+            } while (j <= 35);
+            Console.WriteLine("\nСпособ через For:\n");
+            int k = 25;
+            for(; k<=35; k++)
+            {
+                Console.WriteLine(k + "  " + (k + 0.5) + "  " + (k - 0.2));
+            }
+        }
+    }
+}
