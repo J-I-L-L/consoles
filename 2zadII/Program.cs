d27d0bcddfbe98bb8585e158049c1aedae9e29e9
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-11-30 18:44:17 +0300

    zad2_II

---
+++ 2zadII/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2zadII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num = 0;
+            Console.WriteLine("Напишите число для отображения масти (Число должно быть меньше 4):");
+            byte.TryParse(Console.ReadLine(), out num);
+            switch (num)
+            {
+                case 1: Console.WriteLine("Пики"); break;
+                case 2: Console.WriteLine("Трефы"); break;
+                case 3: Console.WriteLine("Бубны"); break;
+                case 4: Console.WriteLine("Червы"); break;
+                default: Console.WriteLine("Неверное число! Попробуйте ещё раз"); break;
+            }
+        }
+    }
+}
