331386cda891c48c6fdbd3b708dc6f316a7a193c
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-11-30 18:43:39 +0300

    zad2_I

---
+++ 2zadI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _2zadI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x=0.0;
+            double y = 0.0;
+            Console.WriteLine("Введите Х:");
+            double.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine("Введите Y:");
+            double.TryParse(Console.ReadLine(), out y);
+            if(x*x+y*y<64 && x * x + y * y > 9 && x <= 0)
+            {
+                Console.WriteLine("Точка находится внути закрашенной области");
+            } else if(x * x + y * y == 64 || x*x+y*y == 9 && x <= 0)
+            {
+                Console.WriteLine("Точка находится на границе закрашенной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится ВНЕ закрашенной области");
+            }
+        }
+    }
+}
