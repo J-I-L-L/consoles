d7d4a009a34f0f99b521bcfdcac4d1eb8a6e5c94
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-05 11:47:02 +0300

    zad3_I

---
+++ zad3_I/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace zad3_I
+{
+    class Program
+    {
+
+        static double f(double x)
+        {
+            double func = Math.Pow(x, 3) - Math.Sin(x);
+            return func;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для определения наибольшего значения в одной из двух точек");
+            double a = 0;
+            double aX = 0;
+            double b=0;
+            double bX = 0;
+            Console.WriteLine("Введите координату Х точки а:");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Введите координату Х точки b:");
+            double.TryParse(Console.ReadLine(), out b);
+            aX = f(a);
+            bX = f(b);
+            if (aX > bX)
+            {
+                Console.WriteLine("Значение функции больше в точке а");
+            }
+            else
+            {
+                Console.WriteLine("Значение функции выше в точке b");
+            }
+        }
+    }
+}
