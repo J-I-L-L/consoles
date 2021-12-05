d75431c7afa1effb204ba17c524537b66c1be7ff
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-05 13:19:14 +0300

    zad3_II

---
+++ zad3_II/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace zad3_III
+{
+    class Program
+    {
+        static double f(double x, out double y)
+        {
+            y = 0;
+            //double y = 0;
+            if (x >= 0.9)
+            {
+                y = 1 / ((0.1 + x) * (0.1 + x));
+            }
+            else if (x >= 0 && x <= 0.9)
+            {
+                y = 0.2 * x + 0.1;
+            }
+            else if (x < 0)
+            {
+                y = x * x + 0.2;
+            }
+            return y;
+        }
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 0;
+            double h = 0;
+            double otv = 0;
+            Console.WriteLine("Введите а:");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Введите b:");
+            double.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Введите h:");
+            double.TryParse(Console.ReadLine(), out h);
+            for (double i = a; i <= b; i = i + h)
+            {
+                f(i, out otv);
+                Console.WriteLine("f ({0:f1}) = {1:f4}", i, otv);
+            }
+        }
+    }
+}
