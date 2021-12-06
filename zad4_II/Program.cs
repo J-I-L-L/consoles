7bc1781a628c18db682eac31f9b95ce89335a925
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-06 20:37:08 +0300

    zad4_II

---
+++ zad4_II/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace zad4_II
+{
+    class Program
+    {
+        public static void output(int n)
+        {
+            if (n == 1) Console.Write(n+ " ");
+            else
+            {
+                output(n - 1);
+                Console.Write(n+" ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Введите число");
+            Int32.TryParse(Console.ReadLine(), out n);
+            if (n < 1000)
+            {
+                Console.WriteLine("n<1000. Введите другое число");
+            } else
+            {
+                output(n);
+            }
+        }
+    }
+}
