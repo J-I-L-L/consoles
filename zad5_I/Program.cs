5a9e97faf04221fbd22e9b004a6618ed79ee48ef
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-07 17:05:40 +0300

    zad5_I

---
+++ zad5_I/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace zad5_I
+{
+    class Program
+    {
+        static double f(double x)
+        {
+            try
+            {
+                if (x == 0)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    return Math.Sqrt(x * x - 1);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                double a = 0;
+                double b = 0;
+                double h = 0;
+                Console.WriteLine("Введите a:");
+                a=double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите b:");
+                b=double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите h:");
+                h=double.Parse(Console.ReadLine());
+                for (double i = a; i <= b;)
+                {
+                    try
+                    {
+                        Console.WriteLine("y[{0}]= {1:f4}", i, f(i));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("y[{0}] = error", i);
+                    }
+                    i = i + h;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введенно неверное значение. Попробуйте снова.");
+            }
+        }
+    }
+}
+
