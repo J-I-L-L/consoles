1135441e59e21f8115fc495b87ff5d80248ced07
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-13 20:44:40 +0300

    zad8_I

---
+++ zad8_I/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace zad8_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите осмысленное сообщение:");
+            string str="";
+            string[] words;
+            str = Console.ReadLine();
+            words = str.Split(' ', '.', ',', '?', '!', ':', ';');
+            Regex reg = new Regex(@"[А-Я]\S*"); 
+            foreach(var i in reg.Matches(str))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
