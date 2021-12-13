64f705dddc2db5d3e34336bffce8613f31c786ca
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-13 20:43:49 +0300

    zad7_I

---
+++ zad7_I/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace zad7_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerous=0;
+            Console.WriteLine("Введите строку:");
+            string a = new string(Console.ReadLine());
+            string str=a.ToLower();
+            Console.Write("Введите символ х (строчная буква): ");
+            char x = char.Parse(Console.ReadLine());
+            Console.Write("Введите символ у(строчная буква):");
+            char y = char.Parse(Console.ReadLine());
+            for(int i=0; i<a.Length; i++)
+            {
+
+                if (str[i]==x || str[i]==y)
+                {
+                    numerous++;
+                }
+            }
+            Console.WriteLine("Число вхождений символов Х и Y в строке: " +numerous);
+        }
+    }
+}
