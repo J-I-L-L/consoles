49f6ec07b58951a8e96deffb759cb909014e44c3
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-13 20:44:11 +0300

    zad7_II

---
+++ zad7_II/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace zad7_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int buf = 0;
+            string str = "";
+            string[] words;
+            str = Console.ReadLine();
+            words = str.Split();
+            for(int i=0; i<words.Length; i++)
+            {
+                buf = words[i].Length;
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    if (Char.IsDigit(words[i][j]))
+                    {
+                        for(int z=0; z<words[i].Length; z++)
+                        {
+                            if (words[i][z] != ',')
+                                if (words[i][z] != '?')
+                                    if (words[i][z] != '.')
+                                        if (words[i][z] != '!')
+                                            if (words[i][z] != ';')
+                                                Console.Write(words[i][z]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
