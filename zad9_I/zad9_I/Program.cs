5c2719f39fe1fcaa71b264b7d0eb43a2fa458a98
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-14 18:28:47 +0300

    zad9_I

---
+++ zad9_I/zad9_I/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace zad9_I
+{
+    class Program
+    {
+        static int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n:");
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            FileStream f = new FileStream("fib.dat", FileMode.OpenOrCreate);
+            BinaryWriter fOut = new BinaryWriter(f);
+            for(int i=0; i<n; i++)
+            {
+            fOut.Write(Fibonachi(i));
+            }
+            fOut.Close();
+            f = new FileStream("fib.dat", FileMode.Open);
+            BinaryReader fIn = new BinaryReader(f);
+            long m = f.Length;
+            for(int i=0; i<m; i+=4)
+            {
+                f.Seek(i, SeekOrigin.Begin);
+                int hlam = i/4+1;
+                if (hlam % 3 != 0 || i==0)
+                {
+                    int a = fIn.ReadInt32();
+
+                    Console.Write($"{a} ");
+                }
+            }
+            Console.WriteLine();
+            fIn.Close();
+            f.Close();
+        }
+    }
+}
