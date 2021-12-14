0de8a190622dd3c4667550d8306e7c62d6d5e445
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-14 19:41:26 +0300

    zad9_II

---
+++ zad9_II/zad9_II/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace zad9_II
+{
+    class Program
+    {
+        public static string ShortestString(string fileName)
+        {
+            string currentString, shortestString = "";
+            int buf = Int32.MaxValue;
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    currentString = reader.ReadLine();
+                    if (currentString.Length < buf)
+                        buf = shortestString.Length;
+                        shortestString = currentString;
+                }
+                return shortestString;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //StreamReader fileIn = new StreamReader("memory.txt");
+            //string text = fileIn.ReadToEnd();
+            string shortStr = ShortestString("memory.txt");
+            Console.WriteLine("Кратчайшая строка: {0}",shortStr);
+            Console.WriteLine("И её длина: {0}",shortStr.Length);
+           // fileIn.Close();
+        }
+    }
+}
